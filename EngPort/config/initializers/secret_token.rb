168337024cc7f01ea0843f43080ce23171c705d8
danReynolds <fnetma@hotmail.com>
2014-03-28 19:39:45 -0400

    I added everything woot woot

---
+++ EngPort/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EngPort::Application.config.secret_key_base = '73db56511423c091b4a73133626613c5143fc183ca7502cb6c2b33d07eec76e94b48bdc1ad25e75a09024015903b2287893bfc7c58463cf43f47d54090955c5e'
