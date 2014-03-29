8716ac6235dccbd251146aeb7ae2b8ee8e981dc1
Kevin James <KevinJames@thekev.in>
2014-03-29 04:48:39 -0400

    specific exporting

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,30 @@
+var highlighted = new Array();
+
+function clickListener(e) {
+  var clickedElement = (window.event) ? window.event.srcElement : e.target
+  if (clickedElement.tagName == 'A') {
+    pdfExport()
+    return;
+  } else if (clickedElement.tagName != 'DIV') {
+    clickedElement = clickedElement.parentNode
+  }
+
+  for(var j = 0; j < highlighted.length; j++) {
+    if (highlighted[j] == clickedElement.id) {
+      clickedElement.style.backgroundColor = '';
+      highlighted.splice(j, 1);
+      return;
+    }
+  }
+  if (clickedElement.id) {
+    clickedElement.style.backgroundColor = 'gray';
+    highlighted.push(clickedElement.id);
+  }
+  return;
+}
+
+document.onclick = clickListener;
+
+function pdfExport() {
+  window.location = 'announcements/export?ids=' + highlighted
+}
