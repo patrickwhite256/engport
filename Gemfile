dc22d57a5acd611f5973d682e3b2bd53f3aba7e8
danReynolds <fnetma@hotmail.com>
2014-03-28 21:23:37 -0400

    added more

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'jquery-turbolinks'
 
 group :development do
   gem "better_errors"
+  gem 'binding_of_caller'
 end
 
 group :doc do
--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,3 +1,23 @@
-class AnnouncementController < ApplicationController
-  
+class AnnouncementsController < ApplicationController
+
+  def new
+  end
+
+  def index
+    raise
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def show
+    raise
+  end
+
+  def update
+  end
+
 end
