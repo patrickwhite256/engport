3fda1d3754465843333498fbc44be0fcabb11908
danReynolds <fnetma@hotmail.com>
2014-03-29 05:33:47 -0400

    Welcome to full feature search

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -6,7 +6,49 @@ class AnnouncementsController < ApplicationController
   end
 
   def index
-    @announcements = Announcement.all
+    if params[:auto_complete]
+      return render json: [].to_json if params[:q].blank?
+
+      @announcements = []
+
+      @announcements += Announcement.search do
+        fulltext params[:q] do
+          fields :tags
+        end
+      end.results.map do |r|
+        {
+          value: r.title,
+          id: r.id
+        }
+      end
+
+      @announcements += Announcement.search do
+        fulltext params[:q] do
+          fields :description
+        end
+      end.results.map do |r|
+        {
+          value: r.title,
+          id: r.id
+        }
+      end
+
+      @announcements += Announcement.search do
+        fulltext params[:q] do
+          fields :title
+        end
+      end.results.map do |r|
+        {
+          value: r.title,
+          id: r.id
+        }
+      end
+
+      return render json: @announcements.uniq 
+
+    else
+      @announcements = Announcement.all
+    end
   end
 
   def create
--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,3 +1,14 @@
 class Announcement < ActiveRecord::Base
   acts_as_taggable
+
+  searchable do
+    text :title, stored: true
+    text :description, stored: true
+    integer :id
+
+    text :tags do |announcement|
+      announcement.tag_list.join(" ")
+    end
+  end
+
 end
