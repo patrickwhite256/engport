2ad0d618e919caef44c8464901dd84dde9a7a2e1
danReynolds <fnetma@hotmail.com>
2014-03-28 20:54:20 -0400

    added the right stuff

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,2 +1,2 @@
-class PropertiesController < ApplicationController
+class AnnouncementController < ApplicationController
 end
--- db/migrate/20140329002653_add_announcements.rb
+++ db/migrate/20140329002653_add_announcements.rb
@@ -1,6 +1,6 @@
 class AddAnnouncements < ActiveRecord::Migration
   def change
-    create_table :announcemnts do |t|
+    create_table :announcements do |t|
       t.string :description
       t.datetime :date
       t.string :title
