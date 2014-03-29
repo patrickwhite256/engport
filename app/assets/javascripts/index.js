e437b9000ec919d940d7f04b4feacdc42de24eae
danReynolds <fnetma@hotmail.com>
2014-03-29 10:10:35 -0400

    fixed click selection

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -25,24 +25,24 @@ function clickListener(e) {
     return;
   }
 
-  if (clickedElement.parentElement.className == 'event') {
+  if (clickedElement.parentElement.className == 'event visible') {
     clickedElement = clickedElement.parentElement;
-  } else if (clickedElement.className != 'event') {
+  } else if (clickedElement.className != 'event visible') {
     return;
   }
 
   for (var j = 0; j < highlighted.length; j++) {
-    if (highlighted[j] == clickedElement.id) {
+    if (highlighted[j] == $(clickedElement).data('id')) {
       clickedElement.style.backgroundColor = '';
       clickedElement.style.color = '';
       highlighted.splice(j, 1);
       return;
     }
   }
-  if (clickedElement.id) {
+  if ($(clickedElement).data('id')) {
     clickedElement.style.backgroundColor = 'rgba( 255, 50, 0, 0.3 )';
     clickedElement.style.color = 'white';
-    highlighted.push(clickedElement.id);
+    highlighted.push($(clickedElement).data('id'));
   }
   return;
 }
